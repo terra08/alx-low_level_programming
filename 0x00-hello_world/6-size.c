56609fb7476b190c617bde7979eb582260771426
terra08 <ghizlaneessalmi7@gmail.com>
2023-06-25 07:15:16 -0700

    all is done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+int main(void)
+{
+	puts("Size of a char: %d byte(s)\n", sizeof(char));
+	puts("Size of an int: %d byte(s)\n", sizeof(int));
+	puts("Size of a long int: %d byte(s)\n", sizeof(long int));
+	puts("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	puts("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
